f64e2706fd3b63ca9393d7c7af585dc7e2a89aa6
Lexi Anugrah <lexi_anugrah@hotmail.com>
2023-08-08 18:46:10 +0700

    add database seed table

---
+++ database/seed.sql
@@ -0,0 +1,40 @@
+CREATE TABLE "users" (
+  "id" integer PRIMARY KEY,
+  "username" varchar,
+  "access_level" int,
+  "status" varchar,
+  "updated_at" timestamp,
+  "created_at" timestamp
+);
+
+CREATE TABLE "informations" (
+  "id" int PRIMARY KEY,
+  "access_level" int,
+  "title" string,
+  "content" string
+);
+
+CREATE TABLE "authentications" (
+  "id" integer PRIMARY KEY,
+  "user_id" integer,
+  "value" varchar,
+  "type" varchar,
+  "updated_at" timestamp,
+  "created_at" timestamp
+);
+
+CREATE TABLE "logs" ( -- for audit purposes
+  "id" int PRIMARY KEY,
+  "user_id" int,
+  "information_id" int,
+  "ip_address" varchar,
+  "action" varchar,
+  "access_time" timestamp
+);
+
+ALTER TABLE "authentications" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+ALTER TABLE "logs" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+ALTER TABLE "logs" ADD FOREIGN KEY ("information_id") REFERENCES "users" ("id");
+
+CREATE INDEX "index_users_on_username" ON "users" ("username");
+CREATE INDEX "index_informations_on_title" ON "informations" ("title");
